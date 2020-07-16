0e134f240dc8145c7af9a2d6e1b90ec43b638838
AnnaM-Explore <60379811+AnnaM-Explore@users.noreply.github.com>
2020-07-16 14:57:36 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Home","Recommender System", "Top n Recommendations","Solution Overview", "About Us"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -100,9 +100,21 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    if page_selection == "Home":
+        st.title("Welcome!")
+        st.markdown("Some decription with pictures")
+    
+    if page_selection == "Top n recommendations":
+        st.title("Top n movie recommendations")
+        st.markdown("Some decription with pictures")
+
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
+
+    if page_selection == "About us":
+        st.title("Meet the Team")
+        st.info("Team descriptions with pics")
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
