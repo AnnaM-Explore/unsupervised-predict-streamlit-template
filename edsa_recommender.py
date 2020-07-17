314f6eb54cb5c30e86443b1d3888a98b0b91cebb
AnnaM-Explore <60379811+AnnaM-Explore@users.noreply.github.com>
2020-07-17 18:01:55 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -146,7 +146,9 @@ def main():
 
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
-        st.write("Describe how your model works")
+        st.write("Our model works on both the collaborative-based filtering and content-based filtering.")
+        st.write("Collaborative filtering approaches build a model from user’s past behavior (i.e. items purchased or searched by the user) as well as similar decisions made by other users. This model is then used to predict items (or ratings for items) that user may have an interest in.")
+        st.write("Content-based filtering approaches uses a series of discrete characteristics of an item in order to recommend additional items with similar properties. Content-based filtering methods are totally based on a description of the item and a profile of the user’s preferences. It recommends items based on user’s past preferences.")
 
     if page_selection == "EDA":
         st.title("Exploratory Data Analysis")
