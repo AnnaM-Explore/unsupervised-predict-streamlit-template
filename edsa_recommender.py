d152c2209ef7bdc3591f1e2c8541b4a046f51041
AnnaM-Explore <60379811+AnnaM-Explore@users.noreply.github.com>
2020-07-26 23:07:13 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -74,6 +74,9 @@ from recommenders.content_based import content_model
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
 
+# Load raw data
+raw = pd.read_csv("resources/data/movies.csv")
+
 # App declaration
 def main():
 
@@ -161,12 +164,13 @@ def main():
         st.write("* We utilize the similarity measure between user u and user v in this case.")
         st.subheader("Content-based Filtering")
         st.write("* Uses a series of discrete characteristics of an item in order to recommend additional items with similar properties. Content-based filtering methods are totally based on a description of the item and a profile of the user’s preferences. It recommends items based on user’s past preferences.")
-        st.image(colfil)
+        st.image(confil)
 
     if page_selection == "Data":
         st.title("Raw Data")
         st.write("The raw data that was used to make this app possible")
-        st.write("Insert train_df and movie_df heads")
+        st.table(raw.head(10))
+        st.write("There are 100004 rows and 3 columns")
     
     if page_selection == "EDA":
         st.title("Exploratory Data Analysis")
